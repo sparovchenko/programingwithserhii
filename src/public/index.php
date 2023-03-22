17d92509a94e98fa67f537a5e7c4b595362feedb
Serhii Parovchenko <serhii_parovchenko@epam.com>
2023-03-22 22:52:31 +0200

    objects and classes

--- src/public/index.php
+++ src/public/index.php
@@ -2,4 +2,11 @@
 
 declare(strict_types = 1);
 
-echo 'Hello World from Docker!';
+require_once '../http/Transaction.php';
+
+$transaction = new Transaction(100, 'Transaction 1');
+$transaction->addTax(8)->applyDiscount(10);
+
+echo '<pre style="background-color:maroon;color:#00ff00;font-size:20px;padding:10px;">';
+var_dump($transaction->getAmount());
+echo '</pre>';
